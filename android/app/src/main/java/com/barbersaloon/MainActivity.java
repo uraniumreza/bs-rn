29a226ce8bb3aa7bad532ef7742d3afc4185ab3f
Nayeem Reza <reza.uranium@gmail.com>
2019-05-18 00:50:51 +0600

    rename app

--- android/app/src/main/java/com/barbersaloon/MainActivity.java
+++ android/app/src/main/java/com/barbersaloon/MainActivity.java
@@ -1,4 +1,4 @@
-package com.bsrn;
+package com.barbersaloon;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "bsRN";
+        return "BarberSaloon";
     }
 
     @Override
