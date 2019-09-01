8ccdc83c5a66890b984ceaaf8b621247bdb354f4
uraniumreza <reza.uranium@gmail.com>
2019-09-01 20:10:18 +0600

    Update app name :boom:

--- ios/SaloonGalleryTests/SaloonGalleryTests.m
+++ ios/SaloonGalleryTests/SaloonGalleryTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface SalonGalleryTests : XCTestCase
+@interface SaloonGalleryTests : XCTestCase
 
 @end
 
-@implementation SalonGalleryTests
+@implementation SaloonGalleryTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
